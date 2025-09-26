d65432d9962d5f0c93d2c7e29291ec7c36b266e6
Igordutra20 <88938235+Igordutra20@users.noreply.github.com>
2025-09-26 19:13:58 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-
-# Atualizar pip, setuptools e wheel
 python -m pip install --upgrade pip setuptools wheel
-
-# Instalar as dependências do projeto
 pip install -r requirements.txt
